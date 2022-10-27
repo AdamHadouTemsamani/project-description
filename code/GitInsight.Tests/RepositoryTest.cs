a11f703d673ba6e4f2f65df8ed8f555faa5415ad
Adam Hadou Temsamani <ahad@itu.dk>
2022-10-27 12:27:13 +0200

    Changed root folder into code + added docs folder

---
+++ code/GitInsight.Tests/RepositoryTest.cs
@@ -0,0 +1,47 @@
+using LibGit2Sharp;
+using GitInsight;
+using System.Text.RegularExpressions;
+
+namespace GitInsight.Tests;
+
+public class RepositoryTest
+{
+    private string _fullpath;
+    private Repository _repository;
+   
+
+    public RepositoryTest()
+    {
+        var path = Directory.GetParent(Environment.CurrentDirectory)!.Parent!.FullName;
+        string[] extract = Regex.Split(path, "bin");
+        _fullpath = extract[0] + "Testrepo.git";
+        _repository = new Repository(_fullpath);
+    }
+    
+     [Fact]
+    public void Check_If_Correct_Path_Is_Returned()
+    {
+        //Assert
+        _fullpath.Should().Be("C:\\Users\\Adamh\\OneDrive\\Documents\\GitHub\\project-description\\GitInsight\\GitInsight.Tests\\Testrepo.git");
+    }
+
+
+    [Fact]
+    public void Check_If_Repository_Exists_Should_Return_True()
+    {
+        //Arrange & Act
+        var input = Repository.IsValid(_fullpath);
+
+        //Assert
+        input.Should().Be(true);
+
+    }
+
+    [Fact]
+    public void Commits_Should_Equal_Idk()
+    {  
+
+    }
+
+   
+}
---
+++ code/GitInsight/Program.cs
@@ -0,0 +1,45 @@
+﻿using LibGit2Sharp;
+using System.Linq;
+using System;
+
+public static class Program 
+{
+    public static void Main(string[] args)
+    {
+        Repository rep = new Repository(Repository.IsValid(args.FirstOrDefault("")) ? args[0] : Directory.GetParent(Environment.CurrentDirectory)!.Parent!.FullName);
+
+        foreach(var arg in args)
+        {
+            switch(arg)
+            {
+                case "--frequency":
+                    PrintCommitGroupedByFrequency(rep);
+                    break;
+
+                case "--author":
+                    PrintCommitGroupedByDateAndAuthor(rep);
+                    break;
+            }
+        }
+    }
+
+    private static void PrintCommitGroupedByFrequency(Repository com){
+        PrintGitCommitsGroupedByDate(com.Commits.ToList());
+    }
+
+    private static void PrintCommitGroupedByDateAndAuthor(Repository com){
+        com.Commits.GroupBy(x => x.Author.Name).ToList().ForEach(x => {
+            Console.WriteLine(x.Key);
+            PrintGitCommitsGroupedByDate(x.ToList(),true);
+        });
+    }
+
+    private static void PrintGitCommitsGroupedByDate(IList<Commit> group, bool indentation = false){
+        group.ToList().GroupBy(d => d.Author.When.ToString("yyyy-MM-dd")).ToList().ForEach(c => Console.WriteLine((indentation?"\t":"") + c.Count() + " " + c.Key));
+    }
+}
+
+
+
+
+
