71b9aad4c68853708bfaf33ae7f72fca7564b0a1
Adam Hadou Temsamani <ahad@itu.dk>
2022-10-27 15:43:40 +0200

    Finished Testing Program.cs

--- code/GitInsight.Tests/RepositoryTest.cs
+++ code/GitInsight.Tests/RepositoryTest.cs
@@ -36,12 +36,28 @@ public class RepositoryTest
         input.Should().Be(true);
 
     }
-
+    
     [Fact]
-    public void Commits_Should_Equal_Idk()
+    public void Commit_Grouped_By_Frequency()
     {  
+        string answer = "1 2011-04-14\r\n2 2010-05-25\r\n2 2010-05-24\r\n1 2010-05-11 \r\n 1 2010-05-08";
+        StringWriter sw = new StringWriter();
 
+        Console.SetOut(sw);
+        Program.Main(new string[] {_fullpath, "--frequency"});
+
+        sw.ToString().Replace(" ","").TrimEnd().Should().Be(answer.Replace(" ",""));
     }
 
-   
+    [Fact]
+    public void Commit_Grouped_By_Author_and_Date()
+    {
+        string answer = "gor \r\n\t 1 2011-04-14 \r\n Scott Chacon \r\n\t 2 2010-05-25 \r\n\t 2 2010-05-24 \r\n\t 1 2010-05-11 \r\n\t 1 2010-05-08";
+        StringWriter sw = new StringWriter();
+
+        Console.SetOut(sw);
+        Program.Main(new string[] {_fullpath, "--author"});
+
+        sw.ToString().Replace(" ", "").TrimEnd().Should().Be(answer.Replace(" ", ""));
+    }
 }
