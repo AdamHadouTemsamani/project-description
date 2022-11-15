43633134c325911168d48c783d14efeb21c5c0e6
Adam Hadou Temsamani <ahad@itu.dk>
2022-11-15 13:28:29 +0100

    Fixed the bullshit

--- code/GitInsight/Usings.cs
+++ code/GitInsight/Usings.cs
@@ -1,9 +1,7 @@
 global using Microsoft.EntityFrameworkCore;
 global using Core;
 global using Microsoft.AspNetCore.Builder;
-global using Microsoft.AspNetCore;
 global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Configuration;
