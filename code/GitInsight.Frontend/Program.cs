ce912534e934ad798d654856e63bf26489a899cf
COHL7500 <cohl7500@gmail.com>
2022-11-25 14:03:04 +0100

    Change BaseAddress to GitInsight localhost address

--- code/GitInsight.Frontend/Program.cs
+++ code/GitInsight.Frontend/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(_ => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddScoped(_ => new HttpClient {BaseAddress = new Uri("http://localhost:5000")});
 
 await builder.Build()
     .RunAsync();
